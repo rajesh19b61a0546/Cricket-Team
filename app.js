dc5b3151fc9b2ec3fc6f6e6be0d4c59a00bd9fac
Rajesh Sariki <rajeshsariki2707@gmail.com>
2023-05-27 03:38:34 +0000

    Cricket-Teams

--- app.js
+++ app.js
@@ -1,88 +1,78 @@
 const express = require("express");
 const app = express();
-const path=require("path")
+const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const dbPath = path.join(__dirname, "cricketTeam.db");
-const db = null;
-app.use(express.json())
-module.exports = app;
+let db = null;
+app.use(express.json());
 const q = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3009,()=>{
-        console.log("Server Running at http://localhost:3009/")
-    });
+    app.listen(3009, () =>
+      console.log("Server Running at http://localhost:3009/")
+    );
   } catch (e) {
-    console.log(`Db Error: ${e.message}`);
+    console.log(`DB Error: ${e.message}`);
     process.exit(1);
   }
 };
 q();
-const a=(dbObject)=>{
-    return {
-        playerId:dbObject.player_id,
-        playerName:dbObject.player_name,
-        jerseyNumber:dbObject.jersey_number,
-        role:dbObject.role
-
-    }
-}
+const a = (oObject) => {
+  return {
+    playerId: oObject.player_id,
+    playerName: oObject.player_name,
+    jerseyNumber: oObject.jersey_number,
+    role: oObject.role,
+  };
+};
 app.get("/players/", async (request, response) => {
   const player = `select * from cricket_team;`;
-  const play =await db.all(player);
-  response.send(
-      play.map((eachPlayer)=>
-      a(eachPlayer)
-      )
-  );
+  const play = await db.all(player);
+  response.send(play.map((eachPlayer) => a(eachPlayer)));
 });
-app.post("/players/",async (request,response)=>{
-    const playerDetails=request.body;
-    const {playerName,jerseyNumber,role}=playerDetails;
-    const addPlayerQuery=`
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const player_o = `select * from cricket_team WHERE player_id=${playerId};`;
+  response.send(a(await db.get(player_o)));
+});
+app.post("/players/", async (request, response) => {
+  const { playerName, jerseyNumber, role } = request.body;
+  const addPlayerQuery = `
     INSERT INTO
     cricket_team(player_name,jersey_number,role)
     VALUES
     (
-    `${playerName}`,
-    `${jerseyNumber}`,
-    `${role}`);`;
-    const dbResponse=await db.run(addPlayerQuery);
-    response.send("Player Added to Team");
+    "${playerName}",${jerseyNumber},"${role}");`;
+  const dbResponse = await db.run(addPlayerQuery);
+  response.send("Player Added to Team");
 });
-app.get("/players/:playerId", async (request, response) => {
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerName, jerseyNumber, role } = request.body;
   const { playerId } = request.params;
-  const player = `select * from cricket_team WHERE player_id=${playerId};`;
-  const played =await db.get(playerId);
-  response.send(a(played));
-});
-app.put("/players/:playerId",async (request,response)=>{
-   const { playerId } = request.params;
-   const zz=request.body;
-   const {playerName,jerseyNumber,role}=zz;
-   const yy=`
+  const yy = `
    UPDATE
-   cricket_team
+      cricket_team
    SET
-   player_name:`${player_name}`,
-   jersey_number:`${jersey_number}`,
-   role:`${role}`
+     player_name="${player_name}", 
+     jersey_number=${jersey_number},
+     role="${role}"
    WHERE
-   player_id=${playerId};`;
-   await db.run(yy);
-   response.send ("Player Details Updated");
+     player_id=${playerId};`;
+  await db.run(yy);
+  response.send("Player Details Updated");
 });
-app.delete("/players/:playerId",async (request,response)=>{
-     const yyy=`
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const yyy = `
    DELETE FROM 
    cricket_team
    WHERE
    player_id=${playerId};`;
-   await db.run(yyy);
-   response.send("Player Removed");
+  await db.run(yyy);
+  response.send("Player Removed");
 });
-module.exports=app;
+module.exports = app;
